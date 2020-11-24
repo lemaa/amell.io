d44ddc015975e98a8c5a5f51c66a76094aa071e2
amel <amel@3dosft.fr>
2020-11-24 11:00:38 +0100

    Updates

---
+++ precache-manifest.a2a60b675cecbb138c926147e875f6b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cab97513f47569cdfb2076f9945c88",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "9b7276356c84c57bb9f9",
+    "url": "/public/static/css/main.78400dd4.chunk.css"
+  },
+  {
+    "revision": "aa370f9f2b9372ca5a06",
+    "url": "/public/static/js/2.d8590ffb.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/public/static/js/2.d8590ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7276356c84c57bb9f9",
+    "url": "/public/static/js/main.abe19f45.chunk.js"
+  },
+  {
+    "revision": "1500ee8dfd88a92c3fa2",
+    "url": "/public/static/js/runtime-main.e4b6a4d4.js"
+  },
+  {
+    "revision": "2a5d5adaad51bdec8322174ed58171f3",
+    "url": "/public/static/media/MuseoModerno-Bold.2a5d5ada.ttf"
+  },
+  {
+    "revision": "00c7fc7fb2f170ad6c5bf97249bd7900",
+    "url": "/public/static/media/MuseoModerno-VariableFont_wght.00c7fc7f.ttf"
+  }
+]);
