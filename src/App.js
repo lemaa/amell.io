f778d37d90514a70ef7c3c6534699e8fe92f8403
amel <amel@3dosft.fr>
2020-11-24 11:51:39 +0100

    fix url homepage

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ const AnimatedSwitch = withRouter(({ location }) => (
     <TransitionGroup>
       <CSSTransition key={location.key} classNames="slide" timeout={1000}>
         <Switch location={location}>
-          <Route path="/" component={Home} exact />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
+          <Route path="/amell.io/" component={Home} exact />
+          <Route path="/amell.io/contact" component={Contact} />
+          <Route path="/amell.io/resume" component={Resume} />
           <Route path="*" component={NotFound} />
         </Switch>
       </CSSTransition>
@@ -32,19 +32,6 @@ function App() {
           <Global/>
           <BrowserRouter>
         <AnimatedSwitch />
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Go to home</Link>
-            </li>
-            <li>
-              <Link to="/contact">Go to contact</Link>
-            </li>
-            <li>
-              <Link to="/resume">Go to resume</Link>
-            </li>
-          </ul>
-        </nav> */}
       </BrowserRouter>
       </React.Fragment>
       
